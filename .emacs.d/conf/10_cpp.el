f9e9aa6d1619d3a983ab7cbc21d7a7d349df5e37
satojkovic <satojkovic@gmail.com>
2012-02-12 23:24:22 +0900

    customizing indentation

--- .emacs.d/conf/10_cpp.el
+++ .emacs.d/conf/10_cpp.el
@@ -1,10 +1,21 @@
 (setq auto-mode-alist
       (append '(("\\.h$" . c++-mode)
-                ("\\.cpp$" . c++-mode))
+                ("\\.cpp$" . c++-mode)
+                ("\\.c$" . c++-mode))
               auto-mode-alist))
 
 (add-hook 'c++-mode-hook
           '(lambda()
-             (c-set-style "cc-mode")
-             (setq c-tab-always-indent nil)))
+             (setq c-basic-offset 4)
+             (c-set-offset 'substatement-open 0)
+             (c-set-offset 'statement-cont 0)
+             (c-set-offset 'brace-list-open 0)
+             (c-set-offset 'case-label 4)
+             ))
+
+(add-hook 'c++-mode-hook
+          '(lambda()
+             (gtags-mode 1)
+             (gtags-make-complete-list)
+             ))
 
