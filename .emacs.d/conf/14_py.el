f41124e59f830a9ee9238ac6d0a0b14e7062645c
satojkovic <satojkovic@gmail.com>
2012-10-16 23:26:30 +0900

    add python mode settings, python-mode-hook, indent-tabs-mode, tab-width

--- .emacs.d/conf/14_py.el
+++ .emacs.d/conf/14_py.el
@@ -1,2 +1,8 @@
 (require 'ac-python)
 (add-to-list 'ac-modes 'python-mode)
+(add-hook 'python-mode-hook
+          '(lambda()
+             (define-key python-mode-map "\C-m" 'newline-and-indent)))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
