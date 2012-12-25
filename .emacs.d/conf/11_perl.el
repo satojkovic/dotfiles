bb38de30e1ebba5cdf4977c1f07e191da05d6a20
satojkovic <satojkovic@gmail.com>
2012-12-25 22:51:48 +0900

    flymake and completion for perl

--- .emacs.d/conf/11_perl.el
+++ .emacs.d/conf/11_perl.el
@@ -1,5 +1,57 @@
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+
 (defalias 'perl-mode 'cperl-mode)
 (setq cperl-indent-parens-as-block t)
 (setq cperl-indent-level 4)
 (setq cperl-close-paren-offset -4)
 
+(add-hook 'cperl-mode-hook (lambda() (gtags-mode 1)))
+
+;; flymake Perl用設定
+;; http://unknownplace.org/memo/2007/12/21#e001
+(defvar flymake-perl-err-line-patterns
+  '(("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]" 2 3 nil 1)))
+
+(defconst flymake-allowed-perl-file-name-masks
+  '(("\\.pl$" flymake-perl-init)
+    ("\\.pm$" flymake-perl-init)
+    ("\\.t$" flymake-perl-init)))
+
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "perl" (list "-wc" local-file))))
+
+(defun flymake-perl-load ()
+  (interactive)
+  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks (append flymake-allowed-file-name-masks flymake-allowed-perl-file-name-masks))
+  (setq flymake-err-line-patterns flymake-perl-err-line-patterns)
+  (set-perl5lib)
+  (flymake-mode t))
+
+(add-hook 'cperl-mode-hook 'flymake-perl-load)
+
+;; auto-complete
+;; http://d.hatena.ne.jp/IMAKADO/20080612/1213223052
+(add-hook 'cperl-mode-hook
+          (lambda()
+            (require 'perl-completion)
+            (perl-completion-mode t)))
+
+(add-hook 'cperl-mode-hook
+          (lambda()
+            (when (require 'auto-complete nil t)
+              (auto-complete-mode t)
+              (make-variable-buffer-local 'ac-sources)
+              (setq ac-sources
+                    '(ac-source-perl-completion)))))
