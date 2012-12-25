21605f51a23979a7a2a3a53d639f38d0c5f86839
satojkovic <satojkovic@gmail.com>
2012-12-25 23:11:33 +0900

    add iswitchb settings

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -137,3 +137,12 @@
 (setq succor-gtags-enable t)
 (setq succor-imenu-enable t)
 
+;; iswitchb
+(iswitchb-mode 1)
+(add-hook 'iswitchb-define-mode-map-hook
+          (lambda()
+            (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+            (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+            (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+            (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+
