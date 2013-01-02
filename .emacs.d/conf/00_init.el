8c343bb7779beb8977d742c43c45426105825660
satojkovic <satojkovic@gmail.com>
2013-01-02 22:55:01 +0900

    highlight current line

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -174,3 +174,19 @@
                (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
           (message "[%s] %s" line text)))
       (setq count (1- count)))))
+
+;; 
+;; 現在行のハイライト
+;;
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background  "#98FB98"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+(global-hl-line-mode)
