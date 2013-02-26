297dcc4e17471e4997794e0f9cdd4a18eeec570d
satojkovic <satojkovic@gmail.com>
2013-02-27 01:58:59 +0900

    add python mode settings

--- .emacs.d/conf/14_py.el
+++ .emacs.d/conf/14_py.el
@@ -1,8 +1,21 @@
 (require 'ac-python)
-(add-to-list 'ac-modes 'python-mode)
+;(add-to-list 'ac-modes 'python-mode)
+(add-to-list 'ac-modes 'python-2-mode)
 (add-hook 'python-mode-hook
           '(lambda()
              (define-key python-mode-map "\C-m" 'newline-and-indent)))
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (define-key python-mode-map "\"" 'electric-pair)
+            (define-key python-mode-map "\'" 'electric-pair)
+            (define-key python-mode-map "(" 'electric-pair)
+            (define-key python-mode-map "[" 'electric-pair)
+            (define-key python-mode-map "{" 'electric-pair)))
+(defun electric-pair ()
+  "Insert character pair without sournding spaces"
+  (interactive)
+  (let (parens-require-spaces)
+    (insert-pair)))
 
