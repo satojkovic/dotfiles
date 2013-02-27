058ef4d5bbf8da4aa8395106a78ede7d51064e59
satojkovic <satojkovic@gmail.com>
2013-02-28 02:38:17 +0900

    anything config

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -119,7 +119,17 @@
 (require 'auto-install)
 
 ;; anything
-;(require 'anything)
+(require 'anything-config)
+(setq anything-sources (list anything-c-source-buffers
+                             anything-c-source-bookmarks
+                             anything-c-source-recentf
+                             anything-c-source-file-name-history
+                             anything-c-source-locate))
+(define-key anything-map (kbd "C-p") 'anything-previous-line)
+(define-key anything-map (kbd "C-n") 'anything-next-line)
+(define-key anything-map (kbd "C-v") 'anything-next-source)
+(define-key anything-map (kbd "M-v") 'anything-previous-source)
+(global-set-key (kbd "C-;") 'anything)
 
 ;; gtags
 (autoload 'gtags-mode "gtags" "" t)
