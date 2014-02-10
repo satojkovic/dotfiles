114a53bd0b1d87580c05563921355478b802e89a
satojkovic <satojkovic@gmail.com>
2014-02-11 00:12:48 +0900

    change python mode settings

--- .emacs.d/conf/14_py.el
+++ .emacs.d/conf/14_py.el
@@ -37,19 +37,16 @@
 ;;
 ;; flymake-python
 ;;
-(add-to-list 'load-path "~/.emacs.d") ;; check path
-
 (when (load "flymake" t)
-  (defun flymake-pylint-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "~/.emacs.d/pyflymake.py" (list local-file))))
-  ;;     check path
+ (defun flymake-pylint-init ()
+   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                      'flymake-create-temp-inplace))
+          (local-file (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+         (list "pep8" (list "--repeat" local-file))))
 
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
+ (add-to-list 'flymake-allowed-file-name-masks
+              '("\\.py\\'" flymake-pylint-init)))
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
