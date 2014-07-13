fe979ffcc6375523ffb1bdb1bf1a47a9d2b6d6e2
satojkovic <satojkovic@gmail.com>
2014-07-13 17:54:26 +0900

    Add git-gutter settings

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -153,3 +153,14 @@
 
 ;; nyan-mode
 (nyan-mode)
+
+;; git-gutter
+(require 'git-gutter)
+(global-git-gutter-mode t)
+(git-gutter:linum-setup)
+(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
