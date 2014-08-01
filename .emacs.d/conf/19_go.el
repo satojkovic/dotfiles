0feac3a725a12764724fb4088390af0aebb96512
satojkovic <satojkovic@gmail.com>
2014-08-01 15:44:22 +0900

    Add key settings for go source editing

--- .emacs.d/conf/19_go.el
+++ .emacs.d/conf/19_go.el
@@ -3,6 +3,10 @@
           '(lambda()
              (setq c-basic-offset 4)
              (setq indent-tabs-mode t)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+             (local-set-key (kbd "C-c i") 'go-goto-imports)
+             (local-set-key (kbd "C-c d") 'godoc)
              ))
 
 (add-hook 'before-save-hook 'gofmt-before-save)
@@ -14,3 +18,4 @@
 (set-face-attribute 'eldoc-highlight-function-argument nil
                     :underline t :foreground "green"
                     :weight 'bold)
+
