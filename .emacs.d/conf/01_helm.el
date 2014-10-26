b0cb1807ede85b9ef05c55b5ad17a5f7db4304f1
satojkovic <satojkovic@gmail.com>
2014-10-26 14:30:00 +0900

    Add helm keybindings

--- .emacs.d/conf/01_helm.el
+++ .emacs.d/conf/01_helm.el
@@ -1,7 +1,11 @@
 ;; helm
 (require 'helm-config)
 (global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-c u") 'helm-imenu)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (helm-mode 1)
 
 (require 'helm-swoop)
