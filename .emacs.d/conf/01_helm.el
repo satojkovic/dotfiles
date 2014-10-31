67109fa106551b5fd80516518eed9c3f88ec963d
satojkovic <satojkovic@gmail.com>
2014-10-31 22:32:15 +0900

    Add helm settings

--- .emacs.d/conf/01_helm.el
+++ .emacs.d/conf/01_helm.el
@@ -5,6 +5,7 @@
 (global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c u") 'helm-imenu)
+(global-set-key (kbd "C-c g") 'helm-git-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (helm-mode 1)
 
