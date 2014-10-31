ce5a9046811a85d09edf52b2a31d0c689de7b68d
satojkovic <satojkovic@gmail.com>
2014-10-31 22:31:26 +0900

    Add migemo settings

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -195,3 +195,13 @@
 (global-undo-tree-mode t)
 (global-set-key (kbd "M-/") 'undo-tree-redo)
 
+;; migemo
+(require 'migemo)
+(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs" "-i" "\g"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8)
+(load-library "migemo")
+(migemo-init)
