686ce0fe3079a6a9a7a205e1ba24d45158dcda0d
satojkovic <satojkovic@gmail.com>
2015-01-26 23:57:27 +0900

    Update settings for perl-mode

--- .emacs.d/conf/11_perl.el
+++ .emacs.d/conf/11_perl.el
@@ -1,2 +1,8 @@
 (defalias 'perl-mode 'cperl-mode)
 
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (require 'perl-completion)
+            (perl-completion-mode t)
+            (add-to-list 'ac-sources 'ac-source-perl-completion)))
+
