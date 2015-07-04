1cc4bd14a9425e44d5de5f98ed1de6ca30d70895
satojkovic <satojkovic@gmail.com>
2015-07-04 15:02:14 +0900

    Change settings for python-mode

--- .emacs.d/conf/14_py.el
+++ .emacs.d/conf/14_py.el
@@ -1,18 +1,18 @@
 ;;
-;; fundamental settings
+;; python-mode をロードする
 ;;
-(add-hook 'python-mode-hook
-          '(lambda()
-             (define-key python-mode-map "\C-m" 'newline-and-indent)
-             (setq-default indent-tabs-mode nil)
-             (setq python-indent-guess-indent-offset nil)
-             ))
+(when (autoload 'python-mode "python-mode" "Python editing mode." t)
+  ;; python-mode のときのみ python-pep8 のキーバインドを有効にする
+  (setq python-mode-hook
+        (function (lambda ()
+                    (local-set-key "\C-c\ p" 'python-pep8)
+                    (local-set-key "\C-m" 'newline-and-indent))))
+  (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+  (setq interpreter-mode-alist (cons '("python" . python-mode)
+                                     interpreter-mode-alist)))
 
-;;
-;; jedi
-;;
 (require 'epc)
 (require 'python)
 (require 'jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
 (setq jedi:complete-on-dot t)
